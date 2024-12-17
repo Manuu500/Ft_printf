dac7e1517159e3ff7f4379bd470f33acb76fa863
mruiz-ur <espartanmanu1122@gmail.com>
2024-12-18 00:15:33 +0100

    78tyuio

--- ft_printf.c
+++ ft_printf.c
@@ -10,48 +10,63 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 
 int ft_printf(char const *str, ...)
 {
     va_list args;
     size_t  i;
+    size_t  j;
     const   char    *s;
     char    *mem;
 
     va_start(args, str);
     s = str;
     i = 0;
-    mem = malloc(sizeof(char*) * strlen(str) + 1);
-    if (!mem)
-        return (NULL);
+    j = 0;
     while (str[i] != '\0')
     {
-        if (str[i] == '%')
+        if (str[i] == '%' && str[i + 1] == 's')
         {
-            i++;
-            if (str[i] == "s")
+            s = va_arg(args, const char *);
+            if (s != NULL)
             {
-                
+                while (*s)
+                {
+                    write(1, s, 1);
+                    s++;
+                }
+            }
+            i++;
+        }
+        if (str[i] == '%' && str[i + 1] == 'c')
+        {
+            s = va_arg(args, const char);
+            if (s != NULL)
+            {
+                while (*s)
+                {
+                    write(1, s, 1);
+                    s++;
+                }
             }
         }
-        mem[i] = str[i];
+        else
+        {
+            write(1, &str[i], 1);
+        }
         i++;
-    }
-    
-    
-    while (s != NULL)
-    {
-        s = va_arg(args, const char *);
     }
     va_end(args);
 }
 
 int main(void)
 {
-    char word[] = "Hola que tal %s";
-    ft_printf(word, "estas");
+    char word[] = "Hola que tal %s, me ll%cmo manuel";
+    ft_printf(word, "estas", 'a');
 }
